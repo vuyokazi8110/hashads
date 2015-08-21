8bdfcbcc58747995864f41154637f89f0c8b9ee4
hashcode <boniface@hashcode.zm>
2015-08-21 10:03:37 +0200

    Initial commit

---
+++ src/main/java/hashads/App.java
@@ -0,0 +1,15 @@
+package hashads;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+        System.out.println("Hello Im Boniface I added This Line and can the rst add their line");
+    }
+}

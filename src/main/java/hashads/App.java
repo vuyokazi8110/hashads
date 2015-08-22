4ea62564873da676901f95ee06274848d8b342bc
vuyokazi <vuyokaziwondo@yahoo.com>
2015-08-22 19:40:17 +0200

    my first fork

--- src/main/java/hashads/App.java
+++ src/main/java/hashads/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Hello Im Boniface I added This Line and can the rst add their line");
         System.out.println("Hello I'm Sihle (group leader) and I added this line");
+        System.out.println("Hello, Im Vuyo..... I added this line ");
     }
 }
